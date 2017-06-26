0d268a1a54c03888b79ef416b631fded88c2bf99
Daniel Afolabi <dafolabi@fb.com>
2017-06-26 12:04:05 -0700

    added alert controllers for username and password labels

--- parseChat/ViewController.swift
+++ parseChat/ViewController.swift
@@ -7,21 +7,65 @@
 //
 
 import UIKit
+import Parse
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var username: UITextField!
-    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var usernameLabel: UITextField!
+    @IBOutlet weak var passwordLabel: UITextField!
+    
+    let usernameAlertController = UIAlertController(title: "Username Required", message: "Please enter your username.", preferredStyle: .alert)
+     let passwordAlertController = UIAlertController(title: "Password Required", message: "Please enter your password.", preferredStyle: .alert)
     
     
     @IBAction func didHitSignUpButton(_ sender: Any) {
+        
+        // initialize a user object
+        let newUser = PFUser()
+        
+        // set user properties
+        newUser.username = usernameLabel.text
+//        newUser.email = emailLabel.text
+        newUser.password = passwordLabel.text
+        
+        // call sign up function on the object
+        newUser.signUpInBackground { (success: Bool, error: Error?) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                print("User Registered successfully")
+                // manually segue to logged in view
+            }
+        }
     }
     
-    @IBOutlet weak var didHitLoginButton: UIButton!
+    @IBAction func didHitLoginButton(_ sender: Any) {
+        let username = usernameLabel.text ?? ""
+        let password = passwordLabel.text ?? ""
+        
+        if username == "" {
+            self.present(usernameAlertController, animated: true)
+        } else if password == "" {
+            self.present(passwordAlertController, animated: true)
+
+        } else {
+            PFUser.logInWithUsername(inBackground: username, password: password) { (user: PFUser?, error: Error?) in
+                if let error = error {
+                    print("User log in failed: \(error.localizedDescription)")
+                } else {
+                    print("User logged in successfully")
+                    // display view controller that needs to shown after successful login
+                }
+            }
+        }
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        createAlertController()
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,6 +73,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func createAlertController() {
+        // create a cancel action
+        let cancelAction = UIAlertAction(title: "OK", style: .cancel) { (action) in
+            // handle cancel response here. Doing nothing will dismiss the view.
+        }
+        // add the cancel action to the alertController
+        usernameAlertController.addAction(cancelAction)
+        passwordAlertController.addAction(cancelAction)
+    }
 
 }
 
