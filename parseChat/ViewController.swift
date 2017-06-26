7669aa8e3c9b88c3846a6b5ed855e55b398424fc
Daniel Afolabi <dafolabi@fb.com>
2017-06-26 11:45:00 -0700

    created layout for sign in view controller

--- parseChat/ViewController.swift
+++ parseChat/ViewController.swift
@@ -9,6 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    
+    @IBAction func didHitSignUpButton(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var didHitLoginButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
